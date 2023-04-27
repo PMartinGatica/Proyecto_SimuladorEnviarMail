6e96f2015ecd84a2408ab0c430f1f2aa4212dec3
PMartinGatica <pablomartin.gatica@gmail.com>
2023-04-27 14:49:50 -0300

    Cartel Error

--- js/app.js
+++ js/app.js
@@ -12,17 +12,18 @@ document.addEventListener('DOMContentLoaded',function(){
 
     function validar (e){
         if(e.target.value.trim() === ''){
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`)
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`,e.target.parentElement)
         }else{
             console.log('hay algo');
         }
     }
 
-    function mostrarAlerta(mensaje){
+    function mostrarAlerta(mensaje,referencia){
         const error = document.createElement('P');
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white' , 'p2', 'mt-10', 'text-center');
-        formulario.appendChild(error)
+        formulario.appendChild(error);
+        referencia.appendChild(error);
     }
     
 });
